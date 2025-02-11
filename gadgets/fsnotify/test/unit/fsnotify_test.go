592f86fc5f63d41519e6854d0b25f66e1ec2996f
4rivappa <114223788+4rivappa@users.noreply.github.com>
2025-02-11 22:43:37 +0530

    test(test): trigger unit-test to get string values in iMask and faMask

--- gadgets/fsnotify/test/unit/fsnotify_test.go
+++ gadgets/fsnotify/test/unit/fsnotify_test.go
@@ -64,8 +64,8 @@ type ExpectedFsnotifyEvent struct {
 
 	Prio uint32 `json:"prio"`
 
-	FaMask uint32 `json:"fa_mask"`
-	IMask  uint32 `json:"i_mask"`
+	FaMask string `json:"fa_mask"`
+	IMask  string `json:"i_mask"`
 
 	FaType     string `json:"fa_type"`
 	FaPId      uint32 `json:"fa_pid"`
@@ -79,6 +79,21 @@ type ExpectedFsnotifyEvent struct {
 	IInoDir uint32 `json:"i_ino_dir"`
 
 	Name string `json:"name"`
+}
+
+// Print method for ExpectedFsnotifyEvent
+func (e *ExpectedFsnotifyEvent) Print() {
+	fmt.Printf("Timestamp: %s\n", e.Timestamp)
+	fmt.Printf("Type: %s\n", e.Type)
+	fmt.Printf("TraceeProc - PPid: %d, Pid: %d, Tid: %d, Comm: %s, PComm: %s\n", e.TraceeProc.PPid, e.TraceeProc.Pid, e.TraceeProc.Tid, e.TraceeProc.Comm, e.TraceeProc.PComm)
+	fmt.Printf("TracerProc - PPid: %d, Pid: %d, Tid: %d, Comm: %s, PComm: %s\n", e.TracerProc.PPid, e.TracerProc.Pid, e.TracerProc.Tid, e.TracerProc.Comm, e.TracerProc.PComm)
+	fmt.Printf("TraceeMntnsId: %d, TracerMntnsId: %d\n", e.TraceeMntnsId, e.TracerMntnsId)
+	fmt.Printf("TraceeUId: %d, TraceeGId: %d, TracerUId: %d, TracerGId: %d\n", e.TraceeUId, e.TraceeGId, e.TracerUId, e.TracerGId)
+	fmt.Printf("Prio: %d\n", e.Prio)
+	fmt.Printf("FaMask: %d, IMask: %d\n", e.FaMask, e.IMask)
+	fmt.Printf("FaType: %s, FaPId: %d, FaFlags: %d, FaFFlags: %d, FaResponse: %s\n", e.FaType, e.FaPId, e.FaFlags, e.FaFFlags, e.FaResponse)
+	fmt.Printf("IWd: %d, ICookie: %d, IIno: %d, IInoDir: %d\n", e.IWd, e.ICookie, e.IIno, e.IInoDir)
+	fmt.Printf("File Name: %s\n", e.Name)
 }
 
 type testDef struct {
@@ -96,6 +111,10 @@ func TestFsnotifyGadget(t *testing.T) {
 			runnerConfig:  runnerConfig,
 			generateEvent: generateEvent,
 			validateEvent: func(t *testing.T, info *utilstest.RunnerInfo, eventDetails EventDetails, events []ExpectedFsnotifyEvent) {
+				for _, event := range events {
+					event.Print()
+					fmt.Println("----------------------------------------------------------------------------------")
+				}
 				utilstest.ExpectAtLeastOneEvent(func(info *utilstest.RunnerInfo, pid int) *ExpectedFsnotifyEvent {
 					return &ExpectedFsnotifyEvent{
 						Timestamp: utils.NormalizedStr,
